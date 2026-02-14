928e291e60a56c07d47c3f45f97523685cb982ac
Eduardo Miguel Hernandez <emiguelc1991@protonmail.com>
2026-02-14 16:34:33 -0600

    Added initial bed room room

--- game/autoloads/r.gd
+++ game/autoloads/r.gd
@@ -3,13 +3,16 @@ extends "res://addons/popochiu/engine/interfaces/i_room.gd"
 
 # classes ----
 const PRDiningRoom := preload("res://game/rooms/dining_room/room_dining_room.gd")
+const PRBedRoom := preload("res://game/rooms/bed_room/room_bed_room.gd")
 # ---- classes
 
 # nodes ----
 var DiningRoom: PRDiningRoom : get = get_DiningRoom
+var BedRoom: PRBedRoom : get = get_BedRoom
 # ---- nodes
 
 # functions ----
 func get_DiningRoom() -> PRDiningRoom: return get_runtime_room("DiningRoom")
+func get_BedRoom() -> PRBedRoom: return get_runtime_room("BedRoom")
 # ---- functions
 
