8c30b6e9cdf8b67802e3ab883337103fa61f1b55
Eduardo Miguel Hernandez <emiguelc1991@protonmail.com>
2026-03-15 00:15:27 -0500

    Changed  bed and vanity pixel art and added room bath with new interactions

--- game/autoloads/i.gd
+++ game/autoloads/i.gd
@@ -4,14 +4,17 @@ extends "res://addons/popochiu/engine/interfaces/i_inventory.gd"
 # classes ----
 const PIIMakeup := preload("res://game/inventory_items/makeup/inventory_item_makeup.gd")
 const PIILipstick := preload("res://game/inventory_items/lipstick/inventory_item_lipstick.gd")
+const PIIToothbrush := preload("res://game/inventory_items/toothbrush/inventory_item_toothbrush.gd")
 # ---- classes
 
 # nodes ----
 var Makeup: PIIMakeup : get = get_Makeup
 var Lipstick: PIILipstick : get = get_Lipstick
+var Toothbrush: PIIToothbrush : get = get_Toothbrush
 # ---- nodes
 
 # functions ----
 func get_Makeup() -> PIIMakeup: return get_item_instance("Makeup")
 func get_Lipstick() -> PIILipstick: return get_item_instance("Lipstick")
+func get_Toothbrush() -> PIIToothbrush: return get_item_instance("Toothbrush")
 # ---- functions
--- game/autoloads/r.gd
+++ game/autoloads/r.gd
@@ -5,16 +5,19 @@ extends "res://addons/popochiu/engine/interfaces/i_room.gd"
 const PRDiningRoom := preload("res://game/rooms/dining_room/room_dining_room.gd")
 const PRBedRoom := preload("res://game/rooms/bed_room/room_bed_room.gd")
 const PRPrologue := preload("res://game/rooms/prologue/room_prologue.gd")
+const PRBathRoom := preload("res://game/rooms/bath_room/room_bath_room.gd")
 # ---- classes
 
 # nodes ----
 var DiningRoom: PRDiningRoom : get = get_DiningRoom
 var BedRoom: PRBedRoom : get = get_BedRoom
 var Prologue: PRPrologue : get = get_Prologue
+var BathRoom: PRBathRoom : get = get_BathRoom
 # ---- nodes
 
 # functions ----
 func get_DiningRoom() -> PRDiningRoom: return get_runtime_room("DiningRoom")
 func get_BedRoom() -> PRBedRoom: return get_runtime_room("BedRoom")
 func get_Prologue() -> PRPrologue: return get_runtime_room("Prologue")
+func get_BathRoom() -> PRBathRoom: return get_runtime_room("BathRoom")
 # ---- functions
